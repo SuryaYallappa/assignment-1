264da32d796d4cde2026e495e88680112a8fdb9e
SuryaYallappa <suryayallappa@gmail.com>
2025-12-10 19:06:37 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,4 +1,4 @@
 fname=input("Enter your first name: ")
 lname=input("Enter your last name: ")
 
-print("Hello,"+fname+" "+lname+"! Wlcome to the python program")
+print("Hello,",fname," ",lname,"! Wlcome to the python program")
